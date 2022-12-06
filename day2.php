85a4da49b067fda15a72cc0fc5f5839e42711448
YairHtutKhaung35 <119865186+YairHtutKhaung35@users.noreply.github.com>
2022-12-06 15:55:09 +0630

    first commit

--- day2.php
+++ day2.php
@@ -157,6 +157,18 @@ $inv = inv_add($a, $b);
 echo "The total number is :" . $inv;
 echo "<hr>";
 
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
 ?>
 
 
