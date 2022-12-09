af15d7498efda8f7a5bceb1c6d8760be34abdb62
YairHtutKhaung35 <119865186+YairHtutKhaung35@users.noreply.github.com>
2022-12-09 10:58:44 +0630

    Tutorial 03 Review Fix

--- Tutorial_03/index.php
+++ Tutorial_03/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,6 +8,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <div class="position">
         <h1>Enter your birthday</h1>
@@ -15,8 +17,10 @@
             <input type="date" name="date">
             <input type="submit" name="submit" value="submit" class="submit">
         </form>
+
         <?php
-        if (isset($_POST['submit'])) {
+        if (isset($_POST['submit'])) 
+        {
             $submit_date = $_POST['date'];
 
             $userinput_date = new DateTime($submit_date);
@@ -24,16 +28,18 @@
             $current_date = new DateTime();
             //current date time
 
-            //checking if the user input future date to avoid cacuating wrong values
-            if ($userinput_date > $current_date) {
+            //checking if the user input future date and to avoid cacuating wrong values
+            if ($userinput_date > $current_date) 
+            {
                 echo "<p class='error'>Sorry, the day you select is in future .. please choose your birthday</p>";
             } else {
                 $_age = floor((time() - strtotime($submit_date)) / 31556926);
-                //31556926 is 31556926s in a year
+                //31556926 is 31556926 seconds in a year
                 echo "<h3 class='h3'>Your age is : $_age</h3>";
             }
         }
         ?>
     </div>
 </body>
+
 </html>
